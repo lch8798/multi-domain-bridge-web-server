b442675594a68a116249458ed2e0f082dbec27bc
lch8798 <lch8798@gmail.com>
2020-01-12 16:39:47 +0900

    fix. renewal project

--- server.js
+++ server.js
@@ -1,102 +1,59 @@
-const https = require('https');
+const http = require('http');
 const express = require('express');
+const vhost = require('vhost');
+const config = require('./config.json');
+
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const serverConfig = require('./config.json');
 
-const port = serverConfig.http.port;
-let count = 0;
+// redirection https and www
+app.use((req, res, next) => {
+   // https
+   if(config.https.on && !req.secure) {
+      return res.redirect(`https://${req.host}`);
+   }
 
-const webs = [
-    {
-        indexDir: 'web1/build',
-        subDomain: '172.16.15.19',
-    },
-    {
-        indexDir: 'web2/build',
-        subDomain: 'dev.chan.we',
-    },
-];
+   // www
+   if(!req.host.slice(0, req.host.indexOf(config.domain))) {
+      return res.redirect(`https://www.${req.headers.host + req.url}`);
+   }
 
-// https switching
-const httpsServerOn = serverConfig.https.on;
-
-// https redirection
-if (httpsServerOn) {
-    app.enable('trust proxy');
-    app.use(function(req, res, next) {
-        const subDomain = req.host.slice(0, req.host.indexOf('skyload.org'));
-        console.log(subDomain);
-
-        if (!subDomain) {
-            if (req.secure && req.host.indexOf('www.') > -1) {
-                return next();
-            } else {
-                res.redirect(
-                    'https://' +
-                        (req.host.indexOf('www.') == -1 ? 'www.' : '') +
-                        req.headers.host +
-                        req.url
-                );
-            }
-        } else {
-            if (req.secure) {
-                return next();
-            } else {
-                res.redirect('https://' + req.headers.host + req.url);
-            }
-        }
-    });
-}
-
-initDomains(app, webs);
-app.get('*', function(req, res) {
-    console.log(`connect [${count++}] - ${new Date()}`);
-
-    console.log(req.host);
-
-    if (bridge(app, webs, req.host) == false) return;
-    res.render('index.html');
+   next();
 });
 
-app.listen(port, function(req, socket, head) {
-    console.log('listening on port ' + port + '!');
-});
+let webs = [];
+for(let i in config.webs) {
+   if(!config.webs[i].on) continue;
 
-// https server
-let httpsServer = null;
-let httpsOption = {
-    key: 'key',
-    cert: 'cert',
-    ca: 'ca',
-};
-if (httpsServerOn) {
-    httpsOption.key = fs.readFileSync(serverConfig.https.keys.key, 'utf8');
-    httpsOption.cert = fs.readFileSync(serverConfig.https.keys.cert, 'utf8');
-    httpsOption.ca = fs.readFileSync(serverConfig.https.keys.ca, 'utf8');
+   const webInfo = config.webs[i];
+   const web = express();
 
-    httpsServer = https.createServer(httpsOption, app);
-    httpsServer.listen(serverConfig.https.port, () =>
-        console.log(
-            `https server is listening to the port ${serverConfig.https.port}`
-        )
-    );
+   web.use(express.static(webInfo.dir));
+   web.set('view engine', 'ejs');
+   web.engine('html', require('ejs').renderFile);
+   web.get('*', (req, res) => {
+      web.use(express.static(webInfo.dir));
+      web.set('views', webInfo.dir);
+      res.render('index.html');
+   });
+   webs.push(web);
+   app.use(vhost(webInfo.host, web));
 }
 
-function initDomains(expressApp, webs) {
-    expressApp.set('view engine', 'ejs');
-    expressApp.engine('html', require('ejs').renderFile);
+if(webs.length < 1) return console.error('invalid web');
 
-    for (let i in webs) {
-        expressApp.use(express.static(webs[i].indexDir));
-    }
+// run http server
+http.createServer(app).listen(config.http.port);
+
+// run https server
+if(config.https.on) {
+   const https = require('https');
+   const fs = require('fs');
+   const credentials = {
+      key: fs.readFileSync(config.https.keys.key, 'utf8'),
+      cert: fs.readFileSync(config.https.keys.cert, 'utf8'),
+      ca: fs.readFileSync(config.https.keys.ca, 'utf8')
+   }
+
+   https.createServer(credentials, app).listen(config.https.port);
 }
 
-function bridge(expressApp, webs, reqHost) {
-    const host = webs.find(item => item.subDomain == reqHost);
-    if (host == null) return false;
-
-    expressApp.use(express.static('webs/' + host.indexDir));
-    expressApp.set('views', 'webs/' + host.indexDir);
-}
